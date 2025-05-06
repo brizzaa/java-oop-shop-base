6c4ee4733eaf452a2036c5e6235123b1c7a1e419
brizzaa <brizzantex@gmail.com>
2025-05-06 13:47:49 +0200

    feat : adjustments

---
+++ src/org/lessons/java/shop/App.java
@@ -0,0 +1,11 @@
+package org.lessons.java.shop;
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    Prodotto prodotto1 = new Prodotto("Cappuccino", 2.50, "cappuccino cacao");
+    Prodotto prodotto2 = new Prodotto("Pizza", 8, "pizza 100INTEGRALE");
+    System.out.println(prodotto2.getPrezzoTassato());
+    System.out.println(prodotto1.getPrezzoTassato());
+    }
+    
+}
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -2,39 +2,41 @@ package org.lessons.java.shop;
 import java.util.Random;
 
 public class Prodotto {
-    
+
     Random random = new Random();
     public int codiceProdotto;
+
     public String nomeProdotto;
+    public String descrizioneProdotto; 
+    
     public double prezzoProdotto;
-    public double prezzoTassato;
-    public int Iva = 22; 
+    public int ivaProdotto = 22;
 
 
-    public Prodotto(String nome, double prezzo){
-        // se volessi che si salvasse da qualche parte il numero senza generarlo ogni volta?
-        
-        this.codiceProdotto = Math.abs(random.nextInt(1000000)); // è giusto? 
+    public Prodotto(String nome, double prezzo, String descrizione){ 
+        this.codiceProdotto = Math.abs(random.nextInt(100000));   
+        this.descrizioneProdotto = descrizione;
         this.nomeProdotto = nome;
         this.prezzoProdotto = prezzo; 
-        this.prezzoTassato = prezzo + ((prezzo / 100) * Iva);
-
     }
 
 
- 
+
+     
     public double getPrezzo(){
         return this.prezzoProdotto;
     }
 
-    public double getPrezzoTassato(){
-        return this.prezzoTassato;
+
+
+
+     public double getPrezzoTassato(){
+        return this.prezzoProdotto + ((this.prezzoProdotto / 100) * this.ivaProdotto);
     }
     
 
     public String getNomeLungo(){
         return this.codiceProdotto + "-" + this.nomeProdotto; // come funziona il parsing?
     }
-
     
 }
